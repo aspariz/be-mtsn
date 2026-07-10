36be1e67fbae3b755ae278e6e2ff530ec43ed02d
aspariz <rizantaazfa@gmail.com>
2026-07-10 15:30:20 +0700

    redeploy

--- src/controller/uploadController.ts
+++ src/controller/uploadController.ts
@@ -2,8 +2,8 @@ import { Request, Response } from "express";
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
-    process.env.SUPABASE_URL!,
-    process.env.SUPABASE_SERVICE_ROLE_KEY!
+    process.env.VITE_SUPABASE_URL!,
+    process.env.VITE_SUPABASE_SERVICE_ROLE!
 );
 
 const BUCKET = "uploads";
