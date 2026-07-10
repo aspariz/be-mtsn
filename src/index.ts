b32045d51b9a67654e965f94af10f4afd4bac851
aspariz <rizantaazfa@gmail.com>
2026-07-10 10:55:54 +0700

    hhhh

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,8 @@ const port = process.env.PORT || 3000;
 app.use(cors({
     origin: [
         'http://localhost:5173',
-        'https://web-profile-mtsn-kota-tegal.vercel.app' // URL Frontend produksi Anda
+        'https://www.mtsnkotategal.site/',
+        'mtsnkotategal.site' // URL Frontend produksi Anda
     ],
     credentials: true
 }));
